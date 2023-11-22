00cf33d53817f3c0b74a188bffcfbe0312bb6af1
Venkata Sai Nagarjuna Singarapu (vesingar) <vesingar@cisco.com>
2023-11-22 15:59:28 +0530

    react

---
+++ EmployeeDetails/employeedetails/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import EmployeeManagementSystem from './EmployeeManagementSystem';
+
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeManagementSystem />
+    </div>
+  );
+}
+
+
+export default App;
+
---
+++ TimerApplication/timerapplication/src/App.js
@@ -0,0 +1,16 @@
+// App.js
+import React from 'react';
+import Timer from './Timer';
+
+
+const App = () => {
+  return (
+    <div>
+      <Timer />
+    </div>
+  );
+};
+
+
+export default App;
+
